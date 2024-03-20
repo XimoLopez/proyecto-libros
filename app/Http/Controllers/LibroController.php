415f33e088bbad1e487a03cce9cb5d6182fb98a5
Joaquin Lopez <joaquinlopezcrespo@gmail.com>
2024-03-20 06:49:31 +0100

    Adaptaciones finales antes de pasar al punto 4-5 y 6

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -12,7 +12,8 @@ class LibroController extends Controller
     /**
      * Mostar todos los libros
      */
-    public function index(){
+    public function index()
+    {
         $libros = Libro::all();
         return view('libros.index', compact('libros'));
     }
@@ -20,73 +21,79 @@ class LibroController extends Controller
     /**
      * Muestra el formulario para crear un nuevo libro
      */
-    public function create(){
+    public function create()
+    {
         return view('libros.create');
     }
 
     /**
      * Guardar un nuevo libro
      */
-    public function store(Request $request) {
-    // Valida los datos del formulario. Cada campo es requerido y 'disponible' debe ser booleano.
-    $validatedData = $request->validate([
-        'titulo' => 'required', // El título es obligatorio.
-        'autor' => 'required', // El autor es obligatorio.
-        'año_publicacion' => 'required', // El año de publicación es obligatorio.
-        'genero' => 'required', // El género es obligatorio.
-        'disponible' => 'required|boolean' // 'Disponible' es obligatorio y debe ser un valor booleano.
-    ]);
-    // Crea un nuevo registro en la base de datos con los datos validados.
-    Libro::create($validatedData);
-    // Redirige al usuario a la lista de libros.
-    return redirect('/libros')->with('success', 'Libro añadido correctamente.');
+    public function store(Request $request)
+    {
+        // Valida los datos del formulario. Cada campo es requerido y 'disponible' debe ser booleano.
+        $validatedData = $request->validate([
+            'titulo' => 'required', // El título es obligatorio.
+            'autor' => 'required', // El autor es obligatorio.
+            'año_publicacion' => 'required', // El año de publicación es obligatorio.
+            'genero' => 'required', // El género es obligatorio.
+            'disponible' => 'required|boolean' // 'Disponible' es obligatorio y debe ser un valor booleano.
+        ]);
+        // Crea un nuevo registro en la base de datos con los datos validados.
+        Libro::create($validatedData);
+        // Redirige al usuario a la lista de libros.
+        return redirect('/libros')->with('success', 'Libro añadido correctamente.');
     }
-    
+
     /**
      * Muestra el libro
      */
-    public function show(Libro $libro){
+    public function show(Libro $libro)
+    {
         return view('libros.show', compact('libro'));
     }
 
     /**
      * Muestra el formulario para editar el libro
      */
-    public function edit(Libro $libro){   
-    return view('libros.edit', compact('libro'));
+    public function edit(Libro $libro)
+    {
+        return view('libros.edit', compact('libro'));
     }
 
 
     /**
      * Actualizar el libro
      */
-    public function update(Request $request, Libro $libro){
+    public function update(Request $request, Libro $libro)
+    {
         try {
-        $validatedData = $request->validate([
-          'titulo' => 'required',
-              'autor' => 'required',
-              'año_publicacion' => 'required',
-              'genero' => 'required',
-              'disponible' => 'required|boolean'
-          ]);
-  
-          DB::beginTransaction();
-  
-          $libro->update($validatedData);
-  
-          DB::commit();
-  
-          return redirect('/libros')->with('success', 'Libro actualizado correctamente.');
-      } catch (\Exception $e) {
-          DB::rollBack();
-          return redirect()->back()->with('error', 'Error al actualizar el libro. Por favor, inténtalo de nuevo.');
-      }
-  }
+            $validatedData = $request->validate([
+                'titulo' => 'required',
+                'autor' => 'required',
+                'año_publicacion' => 'required',
+                'genero' => 'required',
+                'disponible' => 'required|boolean'
+            ]);
+
+            DB::beginTransaction();
+
+            $libro->update($validatedData);
+
+            DB::commit();
+
+            return redirect('/libros')->with('success', 'Libro actualizado correctamente.');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Error al actualizar el libro. Por favor, inténtalo de nuevo.');
+        }
+    }
     /**
      * Borrar el libro
      */
-    public function destroy(Libro $libro){
-        $libro->delete();//Elimina el libro
-        return redirect('/libros');//Redirige al usuario a la lista de libros
+    public function destroy(Libro $libro)
+    {
+        $libro->delete(); //Elimina el libro
+        return redirect('/libros'); //Redirige al usuario a la lista de libros
     }
 }
--- resources/views/libros/create.blade.php
+++ resources/views/libros/create.blade.php
@@ -1,4 +1,3 @@
-{{-- resources/views/libros/create.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
