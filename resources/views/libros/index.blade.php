cf5c9ed373bc3ee379657cee7fae16904628ac96
Joaquin Lopez <joaquinlopezcrespo@gmail.com>
2024-03-22 13:10:23 +0100

    Modificaiones de usabilidad

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-{{-- Mensaje de éxito y de error --}}
+    {{-- Mensaje de éxito y de error --}}
     @if(session('success'))
         <div class="success-message">{{ session('success') }}</div>
     @endif
@@ -10,17 +10,23 @@
     @if(session('error'))
         <div class="error-message">{{ session('error') }}</div>
     @endif
-<div class="book-list-container">
-    <h1>Libros</h1>
-    <ul>
-        @foreach ($libros as $libro)
-            <li><a href="{{ route('libros.show', $libro) }}">{{ $libro->titulo }}</a></li>
-        @endforeach
-    </ul>
-</div>
-<!-- Botón para añadir un nuevo libro-->
-<div class="add-book-button">
-    <a href="{{ route('libros.create') }}" class="btn btn-primary">Añadir Libro</a>
-</div>
+
+    <div class="book-list-container">
+        <h1>Libros</h1>
+        <ul>
+            @foreach ($libros as $libro)
+                <li>
+                    <div style="margin-bottom: 10px;">
+                        <a href="{{ route('libros.show', $libro) }}">{{ $libro->titulo }}</a>
+                        <span style="float: right; display: block; margin-top: 10px;">{{ $libro->disponible ? 'Disponible' : 'No Disponible' }}</span>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <!-- Botón para añadir un nuevo libro-->
+    <div class="add-book-button">
+        <a href="{{ route('libros.create') }}" class="btn btn-primary">Añadir Libro</a>
+    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LibroController;
 use App\Http\Controllers\PrestamoController;
 
 // Rutas para el controlador de libros
+Route::redirect('/', '/libros');
 Route::get('/libros', [LibroController::class, 'index'])->name('libros.index'); //Muestra todos los libros
 Route::get('/libros/create', [LibroController::class, 'create'])->name('libros.create'); //Muestra el formulario para crear un nuevo libro
 Route::post('/libros', [LibroController::class, 'store'])->name('libros.store'); //Guarda un nuevo libro
