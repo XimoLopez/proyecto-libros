5e6f889163772830c2f2ce3cf7059409c69d5aef
Joaquin Lopez <joaquinlopezcrespo@gmail.com>
2024-04-01 13:37:51 +0200

    Punto 5 - Practica 3 - Vistas de Usuarios y Préstamos

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -7,7 +7,6 @@
             <p>{{ session('success') }}</p>
         </div>
     @endif
-
    @if(session('error'))
     <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
         <p class="font-bold">Error</p>
