f9f26953512fedf2486f7763abcaad024ce473b9
Joaquin Lopez <joaquinlopezcrespo@gmail.com>
2024-04-01 12:11:15 +0200

    Cambiamos los estilos de mensajes a los que ofrece Tailwind

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -2,13 +2,18 @@
 <div class="container">
     {{-- Mensaje de éxito y de error --}}
     @if(session('success'))
-        <div class="success-message">{{ session('success') }}</div>
+        <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4" role="alert">
+            <p class="font-bold">Éxito</p>
+            <p>{{ session('success') }}</p>
+        </div>
     @endif
 
-    @if(session('error'))
-        <div class="error-message">{{ session('error') }}</div>
+   @if(session('error'))
+    <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
+        <p class="font-bold">Error</p>
+        <p>{{ session('error') }}</p>
+    </div>
     @endif
-
     <div class="book-list-container">
         <h1>Libros</h1>
         <ul>
