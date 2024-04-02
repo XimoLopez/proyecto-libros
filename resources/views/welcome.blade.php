0e276d698155b752af5be8400202b3033ff3efcf
Joaquin Lopez <joaquinlopezcrespo@gmail.com>
2024-04-02 06:51:11 +0200

    Modificacion del componente del logo en registro y login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,9 @@
         <div class="flex items-center justify-center min-h-screen">
             <div class="text-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#3182ce" stroke-width="2" style="width: 100px; height: 100px;">
-                <path d="M4 2.5h16c1.1 0 2 .9 2 2v15c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-15c0-1.1.9-2 2-2z"/>
-                <path d="M4 2.5v19.5M20 2.5v19.5"/>
-                <path d="M4 6.5h16M4 10.5h16M4 14.5h16"/>
+                    <path d="M4 2.5h16c1.1 0 2 .9 2 2v15c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-15c0-1.1.9-2 2-2z"/>
+                    <path d="M4 2.5v19.5M20 2.5v19.5"/>
+                    <path d="M4 6.5h16M4 10.5h16M4 14.5h16"/>
                 </svg>
                 <h1 class="text-4xl font-bold text-blue-500 mb-4">Bienvenido a Nuestra Librería Online</h1>
                 <h2 class="text-md text-gray-600 dark:text-gray-400 mb-6">
